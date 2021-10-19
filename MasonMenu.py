13b12a60cb0e6860ff734dcffd2eee894da77660
fnk-E <ethanfunk1115@gmail.com>
2021-10-19 11:34:27 -0400

    added ability to send all 3 menus at the same time

--- MasonMenu.py
+++ MasonMenu.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 ikes = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16653&locationId=27747017&whereami=http://masondining.sodexomyway.com/dining-near-me/ikes"
 southside = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16652&locationId=27747003&whereami=http://masondining.sodexomyway.com/dining-near-me/southside"
 other = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16478&locationId=27747024&whereami=http://masondining.sodexomyway.com/dining-near-me/front-royal"
+
 ikesP = requests.get(ikes)
 ssP = requests.get(southside)
 otherP = requests.get(other)
@@ -22,10 +23,7 @@ menuI = soupI.find("div", id=idCurrent)
 menuS = soupS.find("div", id=idCurrent)
 menuO = soupO.find("div", id=idCurrent)
 
-
-l = menuO.text.split("\n")
-
-print(l)
+menus = [menuI, menuS, menuO]
 
 TOKEN = 'ODkyMTA4ODQ2Mzg0OTUxMzA2.YVIHGw.fbB_55cRs0a4hn0v065apR1b3NE'
 
@@ -40,41 +38,49 @@ async def on_ready():
 
 @tasks.loop(hours=24.0)
 async def called_once_a_day():
-    message_channel = client.get_channel(other)
-    char = 0
-    counter = 0
-    temp = ''
-    flag = 0
-    for i in l:
-        for s in temp.split():
-            char += len(s)
-        if char >= 1000:
-            await message_channel.send(temp)
-            print (temp)
-            if flag == 1:
-                temp = '⠀'
-            else:
-                temp = ''
+    for m in menus:
+        if m == menuI:
+            message_channel = client.get_channel(ikes)
+        elif m == menuS:
+            message_channel = client.get_channel(southside)
+        else: 
+            message_channel = client.get_channel(other)
+        l = m.text.split("\n")
+        print(l)
         char = 0
-        if i.strip() != '':
-            print(repr(i)) 
-            if i.isupper() == True:
-                if i == 'BREAKFAST' or i == 'LUNCH' or i == 'DINNER' or i == 'BRUNCH' or i == 'LATE NIGHT':
-                    temp += '\n ━━━***__' + i + '__***━━━ \n'    
+        counter = 0
+        temp = ''
+        flag = 0
+        for i in l:
+            for s in temp.split():
+                char += len(s)
+            if char >= 1000:
+                await message_channel.send(temp)
+                print (temp)
+                if flag == 1:
+                    temp = '⠀'
                 else:
-                    temp += '\n **' + i + '** \n'      
-            elif counter == 0:
-                flag = 1
-                if temp == '⠀':
-                    temp += i.strip() + '\n'
-                else:
-                    temp += '\t' + i.strip() + '\n'
-                counter += 1
-            elif counter == 1:
-                flag = 0
-                counter = 0 
-    if len(temp) > 0 and temp != '⠀':
-        await message_channel.send(temp)
+                    temp = ''
+            char = 0
+            if i.strip() != '':
+                print(repr(i)) 
+                if i.isupper() == True:
+                    if i == 'BREAKFAST' or i == 'LUNCH' or i == 'DINNER' or i == 'BRUNCH' or i == 'LATE NIGHT':
+                        temp += '\n ━━━***__' + i + '__***━━━ \n'    
+                    else:
+                        temp += '\n **' + i + '** \n'      
+                elif counter == 0:
+                    flag = 1
+                    if temp == '⠀':
+                        temp += i.strip() + '\n'
+                    else:
+                        temp += '\t' + i.strip() + '\n'
+                    counter += 1
+                elif counter == 1:
+                    flag = 0
+                    counter = 0 
+        if len(temp) > 0 and temp != '⠀':
+            await message_channel.send(temp)
     
 @called_once_a_day.before_loop
 async def before():
