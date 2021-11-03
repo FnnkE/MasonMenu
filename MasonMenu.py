9347444a4666c8c580b1e20d46226e708e0ea561
fnk-E <ethanfunk1115@gmail.com>
2021-11-03 14:32:17 -0400

    Started Comments

--- MasonMenu.py
+++ MasonMenu.py
@@ -6,93 +6,107 @@ from discord.ext.commands import has_permissions, MissingPermissions
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+#Menu URLs
 ikes = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16653&locationId=27747017&whereami=http://masondining.sodexomyway.com/dining-near-me/ikes"
 southside = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16652&locationId=27747003&whereami=http://masondining.sodexomyway.com/dining-near-me/southside"
 other = "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=16478&locationId=27747024&whereami=http://masondining.sodexomyway.com/dining-near-me/front-royal"
 
+#Get Pages
 ikesP = requests.get(ikes)
 ssP = requests.get(southside)
 otherP = requests.get(other)
 
+#Convert Pages
 soupI = BeautifulSoup(ikesP.content, "lxml")
 soupS = BeautifulSoup(ssP.content, "lxml")
 soupO = BeautifulSoup(otherP.content, "lxml")
 
+#Calculate Current Day
 currentDay = soupI.find(class_="bite-date current-menu")
 idCurrent = currentDay.get('id') + "-day"
 
+#Limit Page to Current Day
 menuI = soupI.find("div", id=idCurrent)
 menuS = soupS.find("div", id=idCurrent)
 menuO = soupO.find("div", id=idCurrent)
 
-menus = []
-
+#Discord Inits
 TOKEN = 'ODkyMTA4ODQ2Mzg0OTUxMzA2.YVIHGw.fbB_55cRs0a4hn0v065apR1b3NE'
-
 bot = commands.Bot(command_prefix="$")
 
-ikes = 0
-southside = 0
-other = 0
+#Various Inits
+ikes = 0 #Default Channel ID
+southside = 0 #Default Channel ID
+other = 0 #Default Channel ID
+menus = []
+loop = 24 #Default Daily Loop
 
-loop = 24
-
+#Run on Bot Start
 @bot.event
 async def on_ready():
     print('Bot Online')
 
+#Run on $ikes
 @bot.command(name='ikes')
 @has_permissions(manage_channels = True)
 async def ikes(ctx):
     global ikes  
     global loop
-    ikes = ctx.channel.id
-    menus.append(menuI)
-    await ctx.channel.send("Ike's channel has been set")
+    ikes = ctx.channel.id #Get Channel ID
+    menus.append(menuI) #Add Ikes' Menu to List for Printing
+    await ctx.channel.send("Ike's channel has been set") #Confirm Channel Set
+    #Calculate Current Time Till 1AM
     now = datetime.now()
     hour = now.hour
     loop = 25-hour
 
+#Run on $southside
 @bot.command(name='southside')
 @has_permissions(manage_channels = True)
 async def southside(ctx):
     global southside
     global loop
-    southside = ctx.channel.id
-    menus.append(menuS)
-    await ctx.channel.send("Southside's channel has been set")
+    southside = ctx.channel.id #Get Channel ID
+    menus.append(menuS) #Add Southside's Menu to List for Printing
+    await ctx.channel.send("Southside's channel has been set") #Confirm Channel Set
+    #Calculate Current Time Till 1AM
     now = datetime.now()
     hour = now.hour
     loop = 25-hour
 
+#Run on $frontroyale
 @bot.command(name='frontroyale')
 @has_permissions(manage_channels = True)
 async def frontroyale(ctx):
     global other
     global loop
-    other = ctx.channel.id   
-    menus.append(menuO)
-    await ctx.channel.send("Front Royale Common's channel has been set")
+    other = ctx.channel.id #Get Channel ID
+    menus.append(menuO) #Add Front Royale's Menu to List for Printing
+    await ctx.channel.send("Front Royale Common's channel has been set") #Confirm Channel Set
+    #Calculate Current Time Till 1AM
     now = datetime.now()
     hour = now.hour
     loop = 25-hour
 
+#Run on $time
 @bot.command(name='time')
 @has_permissions(manage_channels = True)
 async def timeCheck(ctx):
     message = str(loop) + ' Hours Until Print'
     await ctx.channel.send(message)
 
-
+#Run Daily at 1AM
 @tasks.loop(hours=loop)
 async def called_once_a_day():
     global loop
-    loop = 60
+    loop = 24 #Reset Loop
     for m in menus:
+        #Inits
         char = 0
         counter = 0
         temp = ''
         flag = 0
+        #Print Titles of Menus
         if m == menuI:
             message_channel = bot.get_channel(ikes)
             temp= "⋯⋯⋯⋯⋯| **Ike's** |⋯⋯⋯⋯⋯ \n"
@@ -104,7 +118,9 @@ async def called_once_a_day():
             temp= "⋯⋯⋯⋯⋯| **SMSC Front Royal Commons** |⋯⋯⋯⋯⋯ \n"
         l = m.text.split("\n")
         print(l)
+
         for i in l:
+            #Calc Characters and Send if Needed
             for s in temp.split():
                 char += len(s)
             if char >= 1000:
@@ -115,14 +131,15 @@ async def called_once_a_day():
                 else:
                     temp = ''
             char = 0
+            #Adding Text to List Below
             if i.strip() != '':
                 print(repr(i)) 
-                if i.isupper() == True:
+                if i.isupper() == True: #Add Text Decor
                     if i == 'BREAKFAST' or i == 'LUNCH' or i == 'DINNER' or i == 'BRUNCH' or i == 'LATE NIGHT':
                         temp += '\n ━━━***__' + i + '__***━━━ \n'    
                     else:
                         temp += '\n **' + i + '** \n'      
-                elif counter == 0:
+                elif counter == 0: #I'm going to be honest, i forgot what happens after
                     flag = 1
                     if temp == '⠀':
                         temp += i.strip() + '\n'
@@ -134,7 +151,7 @@ async def called_once_a_day():
                     counter = 0 
         if len(temp) > 0 and temp != '⠀':
             await message_channel.send(temp)
-    
+
 @called_once_a_day.before_loop
 async def before():
     await bot.wait_until_ready()
